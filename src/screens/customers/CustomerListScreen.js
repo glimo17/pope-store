c2647dd97bde920d0332db3d667f6a85911d5842
Jeank <glimo17@gmail.com>
2023-08-11 12:46:00 -0600

    init

--- src/screens/customers/CustomerListScreen.js
+++ src/screens/customers/CustomerListScreen.js
@@ -59,9 +59,12 @@ export default function CustomerListScreen() {
     const fetchData = async () => {
       try {
         dispatch({ type: "FETCH_REQUEST" });
-        const { data } = await axios.get(`/api/customers`, {
-          headers: { Authorization: `Bearer ${userInfo.token}` },
-        });
+        const { data } = await axios.get(
+          `https://pope-api.vercel.app/api/customers`,
+          {
+            headers: { Authorization: `Bearer ${userInfo.token}` },
+          }
+        );
         dispatch({ type: "FETCH_SUCCESS", payload: data });
       } catch (err) {
         dispatch({
