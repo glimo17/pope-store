9775cb9bdf2d886c6d85e747cd07dbf049aecebc
Jeank <glimo17@gmail.com>
2023-08-11 23:23:20 -0600

    initccc

--- src/screens/Accounts/AccountDetailScreen.js
+++ src/screens/Accounts/AccountDetailScreen.js
@@ -79,7 +79,7 @@ export default function AccountDetailScreen() {
     const fetchData = async () => {
       try {
         dispatch({ type: "FETCH_REQUEST" });
-        const { data } = await axios.get(url + `/api/accounts`, {
+        const { data } = await axios.get(url + `api/accounts`, {
           headers: { Authorization: `Bearer ${userInfo.token}` },
         });
         dispatch({ type: "FETCH_SUCCESS", payload: data });
@@ -101,7 +101,7 @@ export default function AccountDetailScreen() {
     if (window.confirm("Are you sure to delete?")) {
       try {
         dispatch({ type: "DELETE_REQUEST" });
-        await axios.delete(url + `/api/customers/${user._id}`, {
+        await axios.delete(url + `api/customers/${user._id}`, {
           headers: { Authorization: `Bearer ${userInfo.token}` },
         });
         toast.success("user deleted successfully");
