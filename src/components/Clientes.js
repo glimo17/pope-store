2320e2ed3a8e1d73f142afd23d5c77fa8b9754b0
Jeank <glimo17@gmail.com>
2023-08-20 21:40:12 -0600

    ooo

--- src/components/Clientes.js
+++ src/components/Clientes.js
@@ -1,18 +1,2 @@
-import { useGrid } from "react-hooks-grid";
-
-const Cliente = ({ data }) => {
-  const { grid, setGrid, rowCount } = useGrid(data);
-  debugger;
-  return (
-    <table>
-      {grid.rows.map((row) => (
-        <tr>
-          {row.cells.map((cell) => (
-            <td>{cell.value}</td>
-          ))}
-        </tr>
-      ))}
-    </table>
-  );
-};
+const Cliente = ({ data }) => {};
 export default Cliente;
